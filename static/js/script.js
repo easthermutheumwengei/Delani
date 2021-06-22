c2ccf5054851fd671d257987ffebe898192cce84
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-06-23 00:35:51 +0300

    add functions o hide and show content under what we do section

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,29 @@ $(document).ready(function() {
         })
     })
 })
+
+
+$(document).ready(function() {
+    $("#dev").hide()
+    $("#dev-img").click(function() {
+        $("#dev").show(1000);
+        $("#dev-img").hide(1000);
+        $("#dev").click(function() {
+            $("#dev-img").show(1000)
+            $("#dev").hide(1000)
+        })
+    })
+})
+
+
+$(document).ready(function() {
+    $("#prod").hide()
+    $("#prod-img").click(function() {
+        $("#prod").show(1000);
+        $("#prod-img").hide(1000);
+        $("#prod").click(function() {
+            $("#prod-img").show(1000)
+            $("#prod").hide(1000)
+        })
+    })
+})
